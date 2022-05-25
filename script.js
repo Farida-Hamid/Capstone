32dca406ae5d89146243e3e2b858720036535df2
Farida-Hamid <faridaaddala@gmail.com>
2022-05-25 20:11:36 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -3,37 +3,37 @@ const actors = [
     image: './images/actor1.png',
     name: 'Mahmoud Al-Ghandour',
     bio: 'Participated in the play entitled (My Love, Who Are You), produced in 2021, where he presented the role of the artist (Abdul Halim Hafez)',
-    role: 'Abdul Halim Hafez'
+    role: 'Abdul Halim Hafez',
   },
   {
     image: './images/actor2.png',
     name: 'Marwa Eid',
     bio: 'An Egyptian actress, a graduate of the Higher Institute of Dramatic Arts, Department of Acting and Directing, she married scriptwriter Mohamed Ezz El Din',
-    role: 'Kariman'
+    role: 'Kariman',
   },
   {
     image: './images/actor3.png',
     name: 'Alhan Al Mahdi',
     bio: 'An Egyptian singer and actress, born in Cairo, entered the National Youth Academy and participated in many works, including "Taht al-Saytara" and "The King"',
-    role: 'So\'ad Hosny'
+    role: 'So\'ad Hosny',
   },
   {
     image: './images/actor4.png',
     name: 'Hala Marzouk',
     bio: 'Egyptian actress, born in 1990 in Cairo, studied at the Higher Institute of Dramatic Arts, highlighted a number of plays, men and women, from the heart to the heart',
-    role: 'Marwa'
+    role: 'Marwa',
   },
   {
     image: './images/actor5.png',
     name: 'Abdel Rahman Al-Qalyubi',
     bio: 'Abdel Rahman Al-Qalyubi is an Egyptian actor, who participated in the Rahim series in 2018, the selection series in 2020, and the movie Musa in 2021',
-    role: 'Mohamed El Mougy'
+    role: 'Mohamed El Mougy',
   },
   {
     image: './images/actor6.png',
     name: 'Ahmed Wahba',
     bio: 'An Egyptian actor, a graduate of the Higher Institute of Theater Arts, participated in more than one dramatic work, including “The Hearts of the series “Al-Zaybak”',
-    role: 'Baligh Hamdi'
+    role: 'Baligh Hamdi',
   },
 ];
 
@@ -42,7 +42,7 @@ function participants(index) {
     image,
     name,
     bio,
-    role
+    role,
   } = actors[index];
 
   const main = document.querySelector('.container');
@@ -81,24 +81,24 @@ open.addEventListener('click', () => {
     <button type='button' class="close-button">X</button>
     `;
 
-    header.appendChild(popup);
-    const close = document.querySelector('.close-button');
-    close.addEventListener('click', () => {
-      header.removeChild(popup);
-    });
-    document.querySelector('#closer1').addEventListener('click', () => {
-      header.removeChild(popup);
-    });
-    document.querySelector('#closer2').addEventListener('click', () => {
-      header.removeChild(popup);
-    });
-    document.querySelector('#closer3').addEventListener('click', () => {
-      header.removeChild(popup);
-    });
-    document.querySelector('#closer4').addEventListener('click', () => {
-      header.removeChild(popup);
-    });
-    document.querySelector('#closer5').addEventListener('click', () => {
-      header.removeChild(popup);
-    });
+  header.appendChild(popup);
+  const close = document.querySelector('.close-button');
+  close.addEventListener('click', () => {
+    header.removeChild(popup);
   });
+  document.querySelector('#closer1').addEventListener('click', () => {
+    header.removeChild(popup);
+  });
+  document.querySelector('#closer2').addEventListener('click', () => {
+    header.removeChild(popup);
+  });
+  document.querySelector('#closer3').addEventListener('click', () => {
+    header.removeChild(popup);
+  });
+  document.querySelector('#closer4').addEventListener('click', () => {
+    header.removeChild(popup);
+  });
+  document.querySelector('#closer5').addEventListener('click', () => {
+    header.removeChild(popup);
+  });
+});
